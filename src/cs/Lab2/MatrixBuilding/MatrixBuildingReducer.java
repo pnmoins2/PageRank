ca1fbd105ff3cd72c62974700d895ea42f015e3f
Valentin GRASSET <33599781+pnmoins2@users.noreply.github.com>
2018-01-08 23:59:18 +0100

    Indentation

--- src/cs/Lab2/MatrixBuilding/MatrixBuildingReducer.java
+++ src/cs/Lab2/MatrixBuilding/MatrixBuildingReducer.java
@@ -12,48 +12,48 @@ public class MatrixBuildingReducer extends Reducer<IntWritable,IntWritable,Text,
 	private Text matrixPosition = new Text();
 	private FloatWritable matrixCoefficient = new FloatWritable();
 	
-    // Overriding of the reduce function
+    	// Overriding of the reduce function
 	// Input : (firstNode, List of arrivalNodes)
 	// Output : ([firstNode,secondNode], matrixCoefficient)
-    @Override
-    protected void reduce(final IntWritable cleI, final Iterable<IntWritable> listevalI, final Context context) throws IOException,InterruptedException
-    {
-    	// Initiate the local variables
-        int currentNode;
-        int arrivalNodeCount = 0;
-        List<Integer> arrivalNodeList = new ArrayList<Integer>();
-        
-        // We count the number of nodes we can reach 
-        Iterator<IntWritable> iterator = listevalI.iterator();
-        while (iterator.hasNext())
-        {
-        	// We recover the arrival node and we store it for the next step
-        	currentNode = iterator.next().get();
-        	arrivalNodeList.add(currentNode);
-        	
-        	// We add this new node in our count
-        	arrivalNodeCount += 1;
-        }
-        
-        // Initiate the local variables for the second step
-        String firstNode = String.valueOf(cleI.get());
-        String secondNode;
-        String indices;
-        
-        // Compute the matrix coefficient for the line
-        matrixCoefficient.set((float) 1 / arrivalNodeCount); 
-        
-        // For each edge (firstNode - secondNode)
-        for(int arrivalNode : arrivalNodeList)
-        {
-        	// Compute the matrix position
-        	secondNode = String.valueOf(arrivalNode);
-        	indices = firstNode + "," + secondNode;
-        	matrixPosition.set(indices);
-        
-    		// Output
-    		context.write(matrixPosition,matrixCoefficient);
-        }
-    }
+    	@Override
+    	protected void reduce(final IntWritable cleI, final Iterable<IntWritable> listevalI, final Context context) throws IOException,InterruptedException
+    	{
+		// Initiate the local variables
+		int currentNode;
+		int arrivalNodeCount = 0;
+		List<Integer> arrivalNodeList = new ArrayList<Integer>();
+
+		// We count the number of nodes we can reach 
+		Iterator<IntWritable> iterator = listevalI.iterator();
+		while (iterator.hasNext())
+		{
+			// We recover the arrival node and we store it for the next step
+			currentNode = iterator.next().get();
+			arrivalNodeList.add(currentNode);
+
+			// We add this new node in our count
+			arrivalNodeCount += 1;
+		}
+
+		// Initiate the local variables for the second step
+		String firstNode = String.valueOf(cleI.get());
+		String secondNode;
+		String indices;
+
+		// Compute the matrix coefficient for the line
+		matrixCoefficient.set((float) 1 / arrivalNodeCount); 
+
+		// For each edge (firstNode - secondNode)
+		for(int arrivalNode : arrivalNodeList)
+		{
+			// Compute the matrix position
+			secondNode = String.valueOf(arrivalNode);
+			indices = firstNode + "," + secondNode;
+			matrixPosition.set(indices);
+
+			// Output
+			context.write(matrixPosition,matrixCoefficient);
+		}
+    	}
 
 }
