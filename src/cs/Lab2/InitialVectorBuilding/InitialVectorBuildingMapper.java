d90ab16adf22af4fea1b95a01e336ec4b8aacc1d
Valentin GRASSET <33599781+pnmoins2@users.noreply.github.com>
2018-01-09 00:03:10 +0100

    Indentation

--- src/cs/Lab2/InitialVectorBuilding/InitialVectorBuildingMapper.java
+++ src/cs/Lab2/InitialVectorBuilding/InitialVectorBuildingMapper.java
@@ -14,21 +14,21 @@ public class InitialVectorBuildingMapper extends Mapper<Text, Text, Text, IntWri
 	// Output : ("node", firstNode), ("node", secondNode)
 	@Override
 	protected void map(Text keyE, Text valE, Context context) throws IOException,InterruptedException
-    {	
+    	{	
 		// Recover the key
-        String key = keyE.toString();
-        
-        // If we are not in the description part
-        if (!key.contains("#"))
-        {
-        	// Recover the first node
-        	nodeNumber.set(Integer.parseInt(key));
-        	context.write(nodeText, nodeNumber);
-        	
-        	// Recover the second node
-        	String val = valE.toString();
-        	nodeNumber.set(Integer.parseInt(val));
-        	context.write(nodeText, nodeNumber);
-        }
-    }
+		String key = keyE.toString();
+
+		// If we are not in the description part
+		if (!key.contains("#"))
+		{
+			// Recover the first node
+			nodeNumber.set(Integer.parseInt(key));
+			context.write(nodeText, nodeNumber);
+
+			// Recover the second node
+			String val = valE.toString();
+			nodeNumber.set(Integer.parseInt(val));
+			context.write(nodeText, nodeNumber);
+		}
+    	}
 }
