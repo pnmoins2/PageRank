9cd223ed086fb7f0b16294ee40674988f704a66a
Valentin GRASSET <33599781+pnmoins2@users.noreply.github.com>
2018-01-08 23:55:58 +0100

    Indentation

--- src/cs/Lab2/PageRank/PageRankReducer.java
+++ src/cs/Lab2/PageRank/PageRankReducer.java
@@ -12,33 +12,33 @@ public class PageRankReducer extends Reducer<IntWritable,FloatWritable,IntWritab
 	private FloatWritable newVectorCoefficient = new FloatWritable();
 	
 	@Override
-    protected void setup(Context context) throws IOException, InterruptedException {
+    	protected void setup(Context context) throws IOException, InterruptedException {
 		dampingFactor = context.getConfiguration().getFloat("dampingFactor", new Float("0.85"));
 		numberOfNodes = context.getConfiguration().getInt("numberOfNodes", 75879);
 	}
 	
-    // Overriding of the reduce function
+    	// Overriding of the reduce function
 	// Input : (index, List of subCoefficients)
 	// Output : (index, newCoeficient)
-    @Override
-    protected void reduce(final IntWritable cleI, final Iterable<FloatWritable> listevalI, final Context context) throws IOException,InterruptedException
-    {
-    	// Initiate the local variables
-        Float sum = new Float("0");
-        Float currentCoefficient;
-        
-        // We sum all the sub coefficients
-        Iterator<FloatWritable> iterator = listevalI.iterator();
-        while (iterator.hasNext())
-        {
-        	currentCoefficient = iterator.next().get();
-        	
-        	sum += currentCoefficient;
-        }
-    	
-    	// Output
-        sum = dampingFactor * sum + (1 - dampingFactor) / numberOfNodes;
-    	newVectorCoefficient.set(sum);
-    	context.write(cleI, newVectorCoefficient);
-    }
+    	@Override
+    	protected void reduce(final IntWritable cleI, final Iterable<FloatWritable> listevalI, final Context context) throws IOException,InterruptedException
+    	{
+		// Initiate the local variables
+		Float sum = new Float("0");
+		Float currentCoefficient;
+
+		// We sum all the sub coefficients
+		Iterator<FloatWritable> iterator = listevalI.iterator();
+		while (iterator.hasNext())
+		{
+			currentCoefficient = iterator.next().get();
+
+			sum += currentCoefficient;
+		}
+
+		// Output
+		sum = dampingFactor * sum + (1 - dampingFactor) / numberOfNodes;
+		newVectorCoefficient.set(sum);
+		context.write(cleI, newVectorCoefficient);
+    	}
 }
