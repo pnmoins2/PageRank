ca1b9d027295c84a8674b58df32f965897b51ece
Valentin GRASSET <33599781+pnmoins2@users.noreply.github.com>
2018-01-09 00:01:28 +0100

    Indentation

--- src/cs/Lab2/InitialVectorBuilding/InitialVectorBuildingReducer.java
+++ src/cs/Lab2/InitialVectorBuilding/InitialVectorBuildingReducer.java
@@ -13,35 +13,35 @@ public class InitialVectorBuildingReducer extends Reducer<Text,IntWritable,IntWr
 	private IntWritable vectorIndex = new IntWritable();
 	private FloatWritable vectorCoefficient = new FloatWritable();
 	
-    // Overriding of the reduce function
+    	// Overriding of the reduce function
 	// Input : ("node", List of nodes)
 	// Output : (nodeNumber, 1/numberOfNodes)
-    @Override
-    protected void reduce(final Text cleI, final Iterable<IntWritable> listevalI, final Context context) throws IOException,InterruptedException
-    {
-    	// Initiate the local variables
-        int currentNode;
-        
-        // We recover each node one time
-        Iterator<IntWritable> firstIterator = listevalI.iterator();
-        while (firstIterator.hasNext())
-        {
-        	currentNode = firstIterator.next().get();
-        	
-        	if (!vectorIndexList.contains(currentNode))
-        	{
-        		vectorIndexList.add(currentNode);
-        	}
-        }
-        
-        // Compute the initial coefficient
-        vectorCoefficient.set((float) 1 / vectorIndexList.size());
-        
-        // We create the initial vector
-        for(int index : vectorIndexList){           
-           vectorIndex.set(index);
-           
-           context.write(vectorIndex, vectorCoefficient);
-        }
-    }
+	@Override
+	protected void reduce(final Text cleI, final Iterable<IntWritable> listevalI, final Context context) throws IOException,InterruptedException
+	{
+		// Initiate the local variables
+		int currentNode;
+
+		// We recover each node one time
+		Iterator<IntWritable> firstIterator = listevalI.iterator();
+		while (firstIterator.hasNext())
+		{
+			currentNode = firstIterator.next().get();
+
+			if (!vectorIndexList.contains(currentNode))
+			{
+				vectorIndexList.add(currentNode);
+			}
+		}
+
+		// Compute the initial coefficient
+		vectorCoefficient.set((float) 1 / vectorIndexList.size());
+
+		// We create the initial vector
+		for(int index : vectorIndexList){           
+			vectorIndex.set(index);
+
+			context.write(vectorIndex, vectorCoefficient);
+		}
+	}
 }
