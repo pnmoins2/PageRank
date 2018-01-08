d126f0f17e8cda0306dbc700857535b1bbf5752b
Valentin GRASSET <33599781+pnmoins2@users.noreply.github.com>
2018-01-08 23:52:52 +0100

    Indentation et changement de nom de variable

--- src/cs/Lab2/Sort/SortReducer.java
+++ src/cs/Lab2/Sort/SortReducer.java
@@ -7,27 +7,27 @@ import java.io.IOException;
 import java.util.Iterator;
 
 public class SortReducer extends Reducer<FloatWritable,Text,Text,FloatWritable> {
-	private Text docIdAndWord = new Text();
-	private FloatWritable tfIdf = new FloatWritable();
+	private Text node = new Text();
+	private FloatWritable pageRank = new FloatWritable();
 	
-    // Overriding of the reduce function
-	// Input : (-tfIdf, List of [word, docId])
-	// Output : ([word, docId], tfIdf)
+    	// Overriding of the reduce function
+	// Input : (-pageRank, List of Nodes)
+	// Output : (node, pageRank)
 	@Override
-    protected void reduce(final FloatWritable cleI, final Iterable<Text> listevalI, final Context context) throws IOException,InterruptedException
-    {
-    	// Recover the tfIdf
-    	tfIdf.set(-cleI.get());
-    	
-        // For each [word, docId]
-        Iterator<Text> iterator = listevalI.iterator();
-        while (iterator.hasNext())
-        {
-        	// Recover the [docId, word]
-        	docIdAndWord.set(iterator.next().toString());
-        	
-        	// Output
-        	context.write(docIdAndWord, tfIdf);
-        }
-    }
+    	protected void reduce(final FloatWritable cleI, final Iterable<Text> listevalI, final Context context) throws IOException,InterruptedException
+    	{
+		// Recover the pageRank
+		pageRank.set(-cleI.get());
+
+		// For each node
+		Iterator<Text> iterator = listevalI.iterator();
+		while (iterator.hasNext())
+		{
+			// Recover the node
+			node.set(iterator.next().toString());
+
+			// Output
+			context.write(node, pageRank);
+		}
+    	}
 }
